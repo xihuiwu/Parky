d2d3985756360f8f83a6edb0cab63034d503cdf6
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-11-02 16:33:01 -0800

    Add Repo For National Park and Trail Modify The _Layout.cshtml To Include JQuery, Toaster, SweetAlert, and FontAwesome

--- ParkyWeb/Models/NationalPark.cs
+++ ParkyWeb/Models/NationalPark.cs
@@ -4,9 +4,9 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ParkyAPI.Models.Dtos
+namespace ParkyWeb.Models
 {
-    public class NationalParkDto
+    public class NationalPark
     {
         public int Id { get; set; }
         [Required]
