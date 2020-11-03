d11281ec58e408bd3b21fa9213d0e18433c58f36
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-11-02 18:10:25 -0800

    Add National Park Controller For Upsert (Update or Insert) Add Corresponding Web Pages

---
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+
+namespace ParkyWeb.Controllers
+{
+    public class NationalParksController : Controller
+    {
+        private readonly INationalParkRepository _npRepo;
+
+        public NationalParksController(INationalParkRepository npRepo)
+        {
+            _npRepo = npRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new NationalPark() { });
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            NationalPark obj = new NationalPark();
+            if (id == null)
+            {
+                return View(obj);
+            }
+            obj = await _npRepo.GetAsync(SD.NationalParkAPIPath, id.GetValueOrDefault());
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _npRepo.GetAsync(SD.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+                if (obj.Id == 0)
+                {
+                    await _npRepo.CreateAsync(SD.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _npRepo.UpdateAsync(SD.NationalParkAPIPath+obj.Id, obj);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+        public async Task<IActionResult> GetAllNationalPark()
+        {
+            return Json(new { data = await _npRepo.GetAllAsync(SD.NationalParkAPIPath) });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteNationalPark(int id)
+        {
+            var status = await _npRepo.DeleteAsync(SD.NationalParkAPIPath, id);
+            if (status)
+            {
+                return Json(new { success = true, message = "Delete Successful" });
+            }
+            return Json(new { success = false, message = "Delete Not Successful" });
+        }
+    }
+}
--- ParkyWeb/Repository/IRepository/IRepository.cs
+++ ParkyWeb/Repository/IRepository/IRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace ParkyWeb.Repository.IRepository
 {
-    interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         Task<T> GetAsync(string url, int id);
         Task<IEnumerable<T>> GetAllAsync(string url);
